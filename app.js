0efa1f54d9e73f61af798f5d6301f4fc8ca5ed1b
Pragnya <exochanyeolyo@gmail.com>
2021-09-02 20:03:45 +0530

    data type array

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-console.log("Learning JS from scratch again ! YAYYYYYYYY");
-console.warn("This a warnin");
-console.error("This an error");
+let arr = [1, 2, 3, 4, 5];
+arr.push("value");
+arr.push(true);
+console.log(arr);
+console.log(typeof arr);
